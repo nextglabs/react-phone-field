5e895c13a48bad3a74c8068d273ccc8e68c8bd53
Bassem <bassem@coradine.com>
2021-03-27 18:48:36 +0100

    eslint fix

--- src/data/territories.ts
+++ src/data/territories.ts
@@ -1,244 +1,220 @@
-import { Territories } from "../typings";
+import { Territories } from '../typings';
 
 export const territories: Territories = {
-    as:
-    {
-        name: 'American Samoa',
-        regions: ['oceania'],
-        iso2: 'as',
-        dialCode: '1684',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    ai:
-    {
-        name: 'Anguilla',
-        regions: ['america', 'caribbean'],
-        iso2: 'ai',
-        dialCode: '1264',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    bm:
-    {
-        name: 'Bermuda',
-        regions: ['america', 'north-america'],
-        iso2: 'bm',
-        dialCode: '1441',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    vg:
-    {
-        name: 'British Virgin Islands',
-        regions: ['america', 'caribbean'],
-        iso2: 'vg',
-        dialCode: '1284',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    ky:
-    {
-        name: 'Cayman Islands',
-        regions: ['america', 'caribbean'],
-        iso2: 'ky',
-        dialCode: '1345',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    ck:
-    {
-        name: 'Cook Islands',
-        regions: ['oceania'],
-        iso2: 'ck',
-        dialCode: '682',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    fk:
-    {
-        name: 'Falkland Islands',
-        regions: ['america', 'south-america'],
-        iso2: 'fk',
-        dialCode: '500',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    fo:
-    {
-        name: 'Faroe Islands',
-        regions: ['europe'],
-        iso2: 'fo',
-        dialCode: '298',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    gi:
-    {
-        name: 'Gibraltar',
-        regions: ['europe'],
-        iso2: 'gi',
-        dialCode: '350',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    gl:
-    {
-        name: 'Greenland',
-        regions: ['america'],
-        iso2: 'gl',
-        dialCode: '299',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    je:
-    {
-        name: 'Jersey',
-        regions: ['europe', 'eu-union'],
-        iso2: 'je',
-        dialCode: '44',
-        mask: '.... ......',
-        priority: null,
-        areaCodes: null
-    },
-    ms:
-    {
-        name: 'Montserrat',
-        regions: ['america', 'caribbean'],
-        iso2: 'ms',
-        dialCode: '1664',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    nu:
-    {
-        name: 'Niue',
-        regions: ['asia'],
-        iso2: 'nu',
-        dialCode: '683',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    nf:
-    {
-        name: 'Norfolk Island',
-        regions: ['oceania'],
-        iso2: 'nf',
-        dialCode: '672',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    mp:
-    {
-        name: 'Northern Mariana Islands',
-        regions: ['oceania'],
-        iso2: 'mp',
-        dialCode: '1670',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    bl:
-    {
-        name: 'Saint Barthélemy',
-        regions: ['america', 'caribbean'],
-        iso2: 'bl',
-        dialCode: '590',
-        mask: null,
-        priority: 1,
-        areaCodes: null
-    },
-    sh:
-    {
-        name: 'Saint Helena',
-        regions: ['africa'],
-        iso2: 'sh',
-        dialCode: '290',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    mf:
-    {
-        name: 'Saint Martin',
-        regions: ['america', 'caribbean'],
-        iso2: 'mf',
-        dialCode: '590',
-        mask: null,
-        priority: 2,
-        areaCodes: null
-    },
-    pm:
-    {
-        name: 'Saint Pierre and Miquelon',
-        regions: ['america', 'north-america'],
-        iso2: 'pm',
-        dialCode: '508',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    sx:
-    {
-        name: 'Sint Maarten',
-        regions: ['america', 'caribbean'],
-        iso2: 'sx',
-        dialCode: '1721',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    tk:
-    {
-        name: 'Tokelau',
-        regions: ['oceania'],
-        iso2: 'tk',
-        dialCode: '690',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    tc:
-    {
-        name: 'Turks and Caicos Islands',
-        regions: ['america', 'caribbean'],
-        iso2: 'tc',
-        dialCode: '1649',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    vi:
-    {
-        name: 'U.S. Virgin Islands',
-        regions: ['america', 'caribbean'],
-        iso2: 'vi',
-        dialCode: '1340',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    },
-    wf:
-    {
-        name: 'Wallis and Futuna',
-        regions: ['oceania'],
-        iso2: 'wf',
-        dialCode: '681',
-        mask: null,
-        priority: null,
-        areaCodes: null
-    }
-}
+  as: {
+    name: 'American Samoa',
+    regions: ['oceania'],
+    iso2: 'as',
+    dialCode: '1684',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  ai: {
+    name: 'Anguilla',
+    regions: ['america', 'caribbean'],
+    iso2: 'ai',
+    dialCode: '1264',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  bm: {
+    name: 'Bermuda',
+    regions: ['america', 'north-america'],
+    iso2: 'bm',
+    dialCode: '1441',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  vg: {
+    name: 'British Virgin Islands',
+    regions: ['america', 'caribbean'],
+    iso2: 'vg',
+    dialCode: '1284',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  ky: {
+    name: 'Cayman Islands',
+    regions: ['america', 'caribbean'],
+    iso2: 'ky',
+    dialCode: '1345',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  ck: {
+    name: 'Cook Islands',
+    regions: ['oceania'],
+    iso2: 'ck',
+    dialCode: '682',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  fk: {
+    name: 'Falkland Islands',
+    regions: ['america', 'south-america'],
+    iso2: 'fk',
+    dialCode: '500',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  fo: {
+    name: 'Faroe Islands',
+    regions: ['europe'],
+    iso2: 'fo',
+    dialCode: '298',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  gi: {
+    name: 'Gibraltar',
+    regions: ['europe'],
+    iso2: 'gi',
+    dialCode: '350',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  gl: {
+    name: 'Greenland',
+    regions: ['america'],
+    iso2: 'gl',
+    dialCode: '299',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  je: {
+    name: 'Jersey',
+    regions: ['europe', 'eu-union'],
+    iso2: 'je',
+    dialCode: '44',
+    mask: '.... ......',
+    priority: null,
+    areaCodes: null,
+  },
+  ms: {
+    name: 'Montserrat',
+    regions: ['america', 'caribbean'],
+    iso2: 'ms',
+    dialCode: '1664',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  nu: {
+    name: 'Niue',
+    regions: ['asia'],
+    iso2: 'nu',
+    dialCode: '683',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  nf: {
+    name: 'Norfolk Island',
+    regions: ['oceania'],
+    iso2: 'nf',
+    dialCode: '672',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  mp: {
+    name: 'Northern Mariana Islands',
+    regions: ['oceania'],
+    iso2: 'mp',
+    dialCode: '1670',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  bl: {
+    name: 'Saint Barthélemy',
+    regions: ['america', 'caribbean'],
+    iso2: 'bl',
+    dialCode: '590',
+    mask: null,
+    priority: 1,
+    areaCodes: null,
+  },
+  sh: {
+    name: 'Saint Helena',
+    regions: ['africa'],
+    iso2: 'sh',
+    dialCode: '290',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  mf: {
+    name: 'Saint Martin',
+    regions: ['america', 'caribbean'],
+    iso2: 'mf',
+    dialCode: '590',
+    mask: null,
+    priority: 2,
+    areaCodes: null,
+  },
+  pm: {
+    name: 'Saint Pierre and Miquelon',
+    regions: ['america', 'north-america'],
+    iso2: 'pm',
+    dialCode: '508',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  sx: {
+    name: 'Sint Maarten',
+    regions: ['america', 'caribbean'],
+    iso2: 'sx',
+    dialCode: '1721',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  tk: {
+    name: 'Tokelau',
+    regions: ['oceania'],
+    iso2: 'tk',
+    dialCode: '690',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  tc: {
+    name: 'Turks and Caicos Islands',
+    regions: ['america', 'caribbean'],
+    iso2: 'tc',
+    dialCode: '1649',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  vi: {
+    name: 'U.S. Virgin Islands',
+    regions: ['america', 'caribbean'],
+    iso2: 'vi',
+    dialCode: '1340',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+  wf: {
+    name: 'Wallis and Futuna',
+    regions: ['oceania'],
+    iso2: 'wf',
+    dialCode: '681',
+    mask: null,
+    priority: null,
+    areaCodes: null,
+  },
+};
--- src/utils/format-number.ts
+++ src/utils/format-number.ts
@@ -1,19 +1,19 @@
 export type FormatNumberOptions = {
-    /** 
-     * allows prefix customization 
-     * @default "+" 
-    */
-    prefix?: string,
-    /**
-     * Enable long numbers to be displayed even if their length is bigger than the phone mask
-     * @default false
-     */
-    enableLongNumbers?: boolean;
-}
+  /**
+   * allows prefix customization
+   * @default "+"
+   */
+  prefix?: string;
+  /**
+   * Enable long numbers to be displayed even if their length is bigger than the phone mask
+   * @default false
+   */
+  enableLongNumbers?: boolean;
+};
 
 /**
  * Formats phone number text according to specified country (applies mask)
- * Example: 
+ * Example:
  * input: `value=123456789` for US gives the following output: `+1 (123) 456-7890`
  * @param value phone number to be formatted
  * @param dialCode international dial code of the country
@@ -21,60 +21,67 @@ export type FormatNumberOptions = {
  * @param options customization options
  * @returns formatted phone number
  */
-export const formatNumber = (value: string, dialCode: string, mask?: string | null, options?: FormatNumberOptions): string => {
-    const defaultOptions = {
-        enableLongNumbers: false,
-        prefix: "+",
-    }
-    const { enableLongNumbers, prefix } = { ...defaultOptions, ...options }
+export const formatNumber = (
+  value: string,
+  dialCode: string,
+  mask?: string | null,
+  options?: FormatNumberOptions
+): string => {
+  const defaultOptions = {
+    enableLongNumbers: false,
+    prefix: '+',
+  };
+  const { enableLongNumbers, prefix } = { ...defaultOptions, ...options };
 
-    if (!value) return prefix;
-    if (!dialCode) return value;
+  if (!value) return prefix;
+  if (!dialCode) return value;
 
-    // TODO: Handle Area Codes
+  // TODO: Handle Area Codes
 
-    // for all values with length less than 2 
-    // OR country doesn't have a defined mask
-    // return the value with prefix
-    if ((value && value.length < 2) || !mask) {
-        return `${prefix}${value}`;
-    }
+  // for all values with length less than 2
+  // OR country doesn't have a defined mask
+  // return the value with prefix
+  if ((value && value.length < 2) || !mask) {
+    return `${prefix}${value}`;
+  }
 
-    const formattedObject = mask.split('').reduce((acc, character) => {
-        if (acc.remainingText.length === 0) {
-            return acc;
-        }
+  const formattedObject = mask.split('').reduce(
+    (acc, character) => {
+      if (acc.remainingText.length === 0) {
+        return acc;
+      }
 
-        if (character !== '.') {
-            return {
-                formattedText: acc.formattedText + character,
-                remainingText: acc.remainingText
-            };
-        }
-
-        const [head, ...tail] = acc.remainingText;
-
+      if (character !== '.') {
         return {
-            formattedText: acc.formattedText + head,
-            remainingText: tail
+          formattedText: acc.formattedText + character,
+          remainingText: acc.remainingText,
         };
-    }, {
-        formattedText: '',
-        remainingText: value.split('')
-    });
+      }
 
+      const [head, ...tail] = acc.remainingText;
 
-    let formattedNumber = formattedObject.formattedText;
-
-    if (enableLongNumbers) {
-        formattedNumber =
-            formattedObject.formattedText + formattedObject.remainingText.join("");
+      return {
+        formattedText: acc.formattedText + head,
+        remainingText: tail,
+      };
+    },
+    {
+      formattedText: '',
+      remainingText: value.split(''),
     }
+  );
 
-    // Always close brackets
-    if (formattedNumber.includes('(') && !formattedNumber.includes(')')) {
-        formattedNumber += ')';
-    }
+  let formattedNumber = formattedObject.formattedText;
 
-    return `${prefix}${dialCode} ${formattedNumber}`;
-}
+  if (enableLongNumbers) {
+    formattedNumber =
+      formattedObject.formattedText + formattedObject.remainingText.join('');
+  }
+
+  // Always close brackets
+  if (formattedNumber.includes('(') && !formattedNumber.includes(')')) {
+    formattedNumber += ')';
+  }
+
+  return `${prefix}${dialCode} ${formattedNumber}`;
+};
