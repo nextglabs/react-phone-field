052a6eda3d5e29597c3db037c62042b189aefd65
Bassem <bassem@coradine.com>
2021-03-27 13:19:34 +0100

    test: Added initial test

--- test/ReactPhone.test.tsx
+++ test/ReactPhone.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as ReactPhone } from '../stories/ReactPhone.stories';
 
-describe('Thing', () => {
+describe('<ReactPhone />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<ReactPhone />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
